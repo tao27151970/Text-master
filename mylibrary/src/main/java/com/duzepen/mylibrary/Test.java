6f03be84eeaa54ba44e9aaf5c5ecfeb36e09c139
wentao <tao27151970@163.com>
2021-09-23 09:58:52 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/duzepen/mylibrary/Test.java
@@ -0,0 +1,16 @@
+package com.duzepen.mylibrary;
+
+/**
+ * 项目名称：Text
+ * 类描述：
+ * 创建人：dell
+ * 创建时间：2019/8/1220:22
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class Test {
+    public double addition(double addnum1,double addnum2){
+        return addnum1 + addnum2;
+    }
+}
